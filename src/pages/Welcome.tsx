23ab2c19effee26a47bdae03cba80ed0c8259ae4
Basurto, Victor {LALA} <victor.basurto@grupolala.com>
2025-09-28 13:45:39 -0600

    configure react router

---
+++ src/pages/Welcome.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Welcome() {
+  return (
+    <div className='w-full h-screen flex justify-center items-center font-bold uppercase'>
+      Welcome
+    </div>
+  )
+}
+
+export default Welcome
